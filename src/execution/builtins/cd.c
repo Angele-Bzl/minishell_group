c47720007742668e14cf2f3a69c8847ba08a4882
Angele Barzilai <abarzila@z4r5p1.42lyon.fr>
2025-05-21 13:17:40 +0200

    cd is working but not normed, also i am supposed to free getcwd() buffer

--- src/execution/builtins/cd.c
+++ src/execution/builtins/cd.c
@@ -2,35 +2,61 @@
 
 #include "../../../header/minishell.h"
 
+// static int	update_pwd(char *variable, int var_length, char *env_line, bool *pwd_exists)
+// {
+// 	if (!ft_strncmp(env_line, variable, var_length))
+// 	{
+// 		if (env_line)
+// 			free(env_line);
+// 		env_line = ft_strjoin(variable, getcwd(NULL, 0));//free getcwd
+// 		if (!env_line)
+// 		{
+// 			ft_putendl_fd("Error: update_pwd failed", STDERR_FILENO);
+// 			return (0);
+// 		}
+// 		*pwd_exists = true;
+// 	}
+// 	return (1);
+// }
 
-static int	update_pwd(t_env **list_env, char *variable, int var_length)
+static int	create_var_pwd(char *env_line, char *variable)
+{
+	env_line = ft_strjoin(variable, getcwd(NULL, 0));//free getcwd
+	if (!env_line)
+	{
+		ft_putendl_fd("Error: update_pwd failed", STDERR_FILENO);
+		return (0);
+	}
+	return (1);
+}
+
+static int	update_env(t_env *list_env, char *variable, int var_length)
 {
 	bool	pwd_exists;
+	t_env	*current;
 
+	current = list_env;
 	pwd_exists = false;
-	while (*list_env)
+	while (current)
 	{
-		if (ft_strncmp((*list_env)->line, variable, var_length))//
+		// if (!update_pwd(variable, var_length, current->line, &pwd_exists))
+		// 	return (0);
+		if (!ft_strncmp(current->line, variable, var_length))
 		{
-			free((*list_env)->line);
-			(*list_env)->line = ft_strjoin(variable, getcwd(NULL, 0));
-			if (!(*list_env)->line)
+			if (current->line)
+				free(current->line);
+			current->line = ft_strjoin(variable, getcwd(NULL, 0));//free getcwd
+			if (!current->line)
 			{
-				// ft_putendl_fd("Error: update_pwd failed", STDERR_FILENO);
+				ft_putendl_fd("Error: update_pwd failed", STDERR_FILENO);
 				return (0);
 			}
 			pwd_exists = true;
 		}
-		*list_env = (*list_env)->next;
+		current = current->next;
 	}
-	if (pwd_exists == true)
-		return (1);
-	(*list_env)->line = ft_strjoin(variable, getcwd(NULL, 0));
-	if (!(*list_env)->line)
-	{
-		// ft_putendl_fd("Error: update_pwd failed", STDERR_FILENO);
-		return (0);
-	}
+	if (pwd_exists == false)
+		return (create_var_pwd(current->line, variable));
 	return (1);
 }
 
@@ -38,66 +64,69 @@ int	exec_cd(char **cmd, t_env *list_env)
 {
 	if (cmd[2])
 	{
-		// ft_putendl_fd("Error: too many arguments", STDERR_FILENO);
+		ft_putendl_fd("Error: too many arguments", STDERR_FILENO);
 		return (0);
 	}
-	/*mettre a jour env oldpwd si elle existe, sinon il faut la creer*/
-	if (!update_pwd(&list_env, "OLDPWD=", 7))
+	if (!update_env(list_env, "OLDPWD=", 7))
 		return (0);
 	if (chdir(cmd[1]) == -1)
 	{
 		perror("Error chdir");
 		return (0);
 	}
-	/*mettre a jour env pwd si la variable existe, sinon il faut la creer*/
-	update_pwd(&list_env, "PWD=", 4);
+	update_env(list_env, "PWD=", 4);
 		return (0);
 	return (1);
 }
-static int  env_init_TEST(t_env **ls_env, char **env)//////////////////
-{
-	unsigned int	i;
-	t_env			*new_node;
+// static int  env_init_TEST(t_env **ls_env, char **env)
+// {
+// 	unsigned int	i;
+// 	t_env			*new_node;
 
-	i = 0;
-	while (env[i])
-	{
-		new_node = malloc(sizeof(t_env));
-		if (!new_node)
-			return (0);
-		new_node->line = ft_strdup(env[i]);
-		if (!new_node->line)
-			return (0);
-		new_node->next = NULL;
-		ft_lstadd_back((t_list**)ls_env, (t_list*)new_node);
-		new_node = new_node->next;
-		i++;
-	}
-	return (1);
-}
+// 	i = 0;
+// 	while (env[i])
+// 	{
+// 		new_node = malloc(sizeof(t_env));
+// 		if (!new_node)
+// 			return (0);
+// 		new_node->line = ft_strdup(env[i]);
+// 		if (!new_node->line)
+// 			return (0);
+// 		new_node->next = NULL;
+// 		ft_lstadd_back((t_list**)ls_env, (t_list*)new_node);
+// 		new_node = new_node->next;
+// 		i++;
+// 	}
+// 	return (1);
+// }
 
+// static void print_env(t_env *ls_env)
+// {
+// 	t_env	*current;
 
-static void print_env(t_env **ls_env)//////////
-{
-	while (*ls_env)
-	{
-		if (ft_strncmp((*ls_env)->line, "PWD=", 4) || ft_strncmp((*ls_env)->line, "OLDPWD=", 7))
-		printf("%s\n", (*ls_env)->line);
-		*ls_env = (*ls_env)->next;
-	}
-}
-int main(int ac, char **av, char **env)
-{
-	t_env 	*ls_env;
-	char 	*cmds[2] = {"cd", "../.."};
+// 	current = ls_env;
+// 	while (current)
+// 	{
+// 		if (!ft_strncmp((current)->line, "PWD=", 4) || !ft_strncmp((current)->line, "OLDPWD=", 7))
+// 		printf("%s\n", (current)->line);
+// 		current = (current)->next;
+// 	}
+// }
 
-	ls_env = malloc(sizeof(t_env));
-	env_init_TEST(&ls_env, env);
-	printf("FIRST\n\n");
-	print_env(&ls_env);
-	printf("hello---------------\n");
-	exec_cd(cmds, ls_env);
-	printf("--------------------------------------------------\n\n");
-	printf("SECOND\n\n");
-	print_env(&ls_env);
-}
+// int main(int ac, char **av, char **env)
+// {
+// 	t_env 	*ls_env;
+// 	char 	*cmds[3] = {"cd", "../..", NULL};
+
+// 	ls_env = NULL;
+// 	env_init_TEST(&ls_env, env);
+// 	printf("FIRST\n\n");
+// 	print_env(ls_env);
+// 	printf("cwd = %s\n", getcwd(NULL, 0));
+// 	printf("---------------\n");
+// 	exec_cd(cmds, ls_env);
+// 	printf("---------------\n");
+// 	printf("SECOND\n\n");
+// 	print_env(ls_env);
+// 	printf("cwd = %s\n", getcwd(NULL, 0));
+// }
