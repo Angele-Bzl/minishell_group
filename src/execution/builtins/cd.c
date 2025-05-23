a1223211118df10ba67871b67987bd61f3bf14ad
Angele Barzilai <abarzila@z4r5p1.42lyon.fr>
2025-05-23 09:19:46 +0200

    update cd with oldpwd

--- src/execution/builtins/cd.c
+++ src/execution/builtins/cd.c
@@ -24,7 +24,7 @@ static int	create_var_pwd(char *env_line, char *variable)
 	char *pwd;
 
 	pwd = getcwd(NULL, 0);
-	env_line = ft_strjoin(variable, pwd);//free getcwd
+	env_line = ft_strjoin(variable, pwd);
 	free(pwd);
 	if (!env_line)
 	{
@@ -34,7 +34,7 @@ static int	create_var_pwd(char *env_line, char *variable)
 	return (1);
 }
 
-static int	update_env(t_env *list_env, char *variable, int var_length)
+static int	update_pwd(t_env *list_env, char *variable, int var_length)
 {
 	bool	pwd_exists;
 	t_env	*current;
@@ -53,9 +53,10 @@ static int	update_env(t_env *list_env, char *variable, int var_length)
 			pwd = getcwd(NULL, 0);
 			if (!pwd)
 			{
-				/*fail*/
+				ft_putendl_fd("Error: update_pwd failed", STDERR_FILENO);
+				return (0);
 			}
-			current->line = ft_strjoin(variable, pwd);//free getcwd
+			current->line = ft_strjoin(variable, pwd);
 			if (!current->line)
 			{
 				ft_putendl_fd("Error: update_pwd failed", STDERR_FILENO);
@@ -71,6 +72,49 @@ static int	update_env(t_env *list_env, char *variable, int var_length)
 	return (1);
 }
 
+static int	update_oldpwd(t_env *ls_env)
+{
+	bool	pwd_exists;
+	t_env	*current;
+	char	*pwd;
+
+	current = ls_env;
+	pwd = NULL;
+	while (current)
+	{
+		if (!ft_strncmp(current->line, "PWD=", 4))
+		{
+			pwd = ft_strdup(current->line + 4);
+			if (!pwd)
+			{
+				ft_putendl_fd("Error: update_oldpwd failed", STDERR_FILENO);
+				return (0);
+			}
+			break ;
+		}
+		current = current->next;
+	}
+	current = ls_env;
+	while (current)
+	{
+		if (!ft_strncmp(current->line, "OLDPWD=", 7))
+		{
+			free(current->line);
+			current->line = ft_strjoin("OLDPWD=", pwd);
+			if (!current->line)
+			{
+				free(pwd);
+				ft_putendl_fd("Error: update_oldpwd failed", STDERR_FILENO);
+				return (0);
+			}
+		}
+		current = current->next;
+	}
+	if (pwd_exists == false)
+		return (create_var_pwd(current->line, "OLDPWD="));
+	return (1);
+}
+
 int	exec_cd(char **cmd, t_env *list_env)
 {
 	if (cmd[2])
@@ -78,14 +122,14 @@ int	exec_cd(char **cmd, t_env *list_env)
 		ft_putendl_fd("Error: too many arguments", STDERR_FILENO);
 		return (0);
 	}
-	if (!update_env(list_env, "OLDPWD=", 7))
+	if (!update_oldpwd(list_env))
 		return (0);
 	if (chdir(cmd[1]) == -1)
 	{
 		perror("Error chdir");
 		return (0);
 	}
-	update_env(list_env, "PWD=", 4);
+	update_pwd(list_env, "PWD=", 4);
 		return (0);
 	return (1);
 }
--- src/execution/builtins/unset.c
+++ src/execution/builtins/unset.c
@@ -1,6 +1,6 @@
-// # include "minishell.h"
+# include "minishell.h"
 
-#include "../../../header/minishell.h"
+// #include "../../../header/minishell.h"
 
 // static void	check_and_update(char *cmd, t_env *current, t_env *previous, t_env **ls_env)
 // {
@@ -61,75 +61,75 @@ int	exec_unset(t_env **ls_env, char **cmds)
 	return (1);
 }
 
-static int  env_init_TEST(t_env **ls_env, char **env)
-{
-	unsigned int	i;
-	t_env			*new_node;
-	i = 0;
-	while (env[i])
-	{
-		new_node = malloc(sizeof(t_env));
-		if (!new_node)
-			return (0);
-		new_node->line = ft_strdup(env[i]);
-		if (!new_node->line)
-			return (0);
-		new_node->next = NULL;
-		ft_lstadd_back((t_list**)ls_env, (t_list*)new_node);
-		new_node = new_node->next;
-		i++;
-	}
-	return (1);
-}
+// static int  env_init_TEST(t_env **ls_env, char **env)
+// {
+// 	unsigned int	i;
+// 	t_env			*new_node;
+// 	i = 0;
+// 	while (env[i])
+// 	{
+// 		new_node = malloc(sizeof(t_env));
+// 		if (!new_node)
+// 			return (0);
+// 		new_node->line = ft_strdup(env[i]);
+// 		if (!new_node->line)
+// 			return (0);
+// 		new_node->next = NULL;
+// 		ft_lstadd_back((t_list**)ls_env, (t_list*)new_node);
+// 		new_node = new_node->next;
+// 		i++;
+// 	}
+// 	return (1);
+// }
 
-static void print_env(t_env *ls_env)
-{
-	t_env	*current;
-	current = ls_env;
-	while (current)
-	{
-		// if (!ft_strncmp((current)->line, "A=a", 3) || !ft_strncmp((current)->line, "Z=z", 3))
-			printf("%s\n", (current)->line);
-		current = (current)->next;
-	}
-}
+// static void print_env(t_env *ls_env)
+// {
+// 	t_env	*current;
+// 	current = ls_env;
+// 	while (current)
+// 	{
+// 		// if (!ft_strncmp((current)->line, "A=a", 3) || !ft_strncmp((current)->line, "Z=z", 3))
+// 			printf("%s\n", (current)->line);
+// 		current = (current)->next;
+// 	}
+// }
 
 
-int	exec_export(t_env *ls_env, char **cmds)
-{
-	int		i;
-	t_env	*new;
+// int	exec_export(t_env *ls_env, char **cmds)
+// {
+// 	int		i;
+// 	t_env	*new;
 
-	i = 0;
-	while (cmds[i])
-	{
-		new = malloc(sizeof(t_env));
-		if (!new)
-		{
-			ft_putendl_fd("Error exec_export: malloc failed", STDERR_FILENO);
-			return (0);
-		}
-		new->line = cmds[i];
-		new->next = NULL;
-		ft_lstadd_back((t_list**)&ls_env, (t_list*)new);
-		i++;
-	}
-	return (1);
-}
-int main(int ac, char **av, char **env)
-{
-	t_env 	*ls_env;
-	// char 	*cmds[4] = {"export", "A=a", "Z=z", NULL};
-	char	*cmds_unset[3]={"unset", "SYSTEMD_EXEC_PID", NULL};
-	ls_env = NULL;
-	env_init_TEST(&ls_env, env);
-	// printf("before export----------------------------\n");
-	// print_env(ls_env);
-	// exec_export(ls_env, cmds);
-	// printf("after export--------------------------------\n");
-	// print_env(ls_env);
-	printf("before unset--------------------------------\n");
-	exec_unset(&ls_env, cmds_unset);
-	printf("after unset--------------------------------\n");
-	// print_env(ls_env);
-}
+// 	i = 0;
+// 	while (cmds[i])
+// 	{
+// 		new = malloc(sizeof(t_env));
+// 		if (!new)
+// 		{
+// 			ft_putendl_fd("Error exec_export: malloc failed", STDERR_FILENO);
+// 			return (0);
+// 		}
+// 		new->line = cmds[i];
+// 		new->next = NULL;
+// 		ft_lstadd_back((t_list**)&ls_env, (t_list*)new);
+// 		i++;
+// 	}
+// 	return (1);
+// }
+// int main(int ac, char **av, char **env)
+// {
+// 	t_env 	*ls_env;
+// 	// char 	*cmds[4] = {"export", "A=a", "Z=z", NULL};
+// 	char	*cmds_unset[3]={"unset", "PWD", NULL};
+// 	ls_env = NULL;
+// 	env_init_TEST(&ls_env, env);
+// 	// printf("before export----------------------------\n");
+// 	// print_env(ls_env);
+// 	// exec_export(ls_env, cmds);
+// 	// printf("after export--------------------------------\n");
+// 	// print_env(ls_env);
+// 	printf("before unset--------------------------------\n");
+// 	exec_unset(&ls_env, cmds_unset);
+// 	printf("after unset--------------------------------\n");
+// 	// print_env(ls_env);
+// }
