1d951922b6dee1012f001bee9b06cfd926a0ec0d
Mathis Ballet <maballet@student.42lyon.fr>
2025-06-11 16:16:23 +0200

    last fix updates

--- src/parsing/expand_var.c
+++ src/parsing/expand_var.c
@@ -1,5 +1,20 @@
 # include "minishell.h"
 
+int	is_expandable(char current_i, char next_i, t_parsing *parsing)
+{
+	if (current_i != '$')
+		return (0);
+	if (current_i == '$' && parsing->simple_quote == false)
+		return (1);
+	if (!ft_isspace(next_i))
+		return (1);
+	if (next_i != '\0')
+		return (1);
+	if (next_i == '\"' && parsing->double_quote == true)
+		return (0);
+	return (0);
+}
+
 void	quote_check(char c, t_parsing *parsing)
 {
 	if (c == '\"' && parsing->double_quote == false && parsing->simple_quote == false)
@@ -21,7 +36,7 @@ int	dollar_remaining(char *str, t_parsing *parsing)
 	{
 		quote_check(str[i], parsing);
 		if (str[i] == '$' && parsing->simple_quote == false && !ft_isspace(str[i + 1])
-			&& str[i + 1] != '\0')
+			&& str[i + 1] != '\0' && str[i + 1] != '\"')
 			return (1);
 		i++;
 	}
@@ -37,9 +52,8 @@ int	expand_var(t_data *data, t_parsing *parsing)										// partie expand, "go!
 		while (parsing->prompt_tab[parsing->pipe_seg][parsing->p_index])				// tant qu'on a des charactères dans le seg_pipe actuel
 		{
 			quote_check(parsing->prompt_tab[parsing->pipe_seg][parsing->p_index], parsing);
-			if (parsing->prompt_tab[parsing->pipe_seg][parsing->p_index] == '$' && parsing->simple_quote == false
-				&& !ft_isspace(parsing->prompt_tab[parsing->pipe_seg][parsing->p_index + 1])
-				&& parsing->prompt_tab[parsing->pipe_seg][parsing->p_index + 1] != '\0')
+			if (is_expandable(parsing->prompt_tab[parsing->pipe_seg][parsing->p_index],
+				parsing->prompt_tab[parsing->pipe_seg][parsing->p_index + 1], parsing))
 			{
 				if (manage_dollar(data, parsing) == -1)									// créer un nouveau prompt avec le contenu de la var croisée
 					return (-1);
