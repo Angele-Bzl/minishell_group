dba57266ffdd5639c6ef479ffe04c75a0052e865
Angele Barzilai <abarzila@z1r11p5.42lyon.fr>
2025-07-03 14:08:18 +0200

    cd invalid-arg OK

--- src/execution/error_exec.c
+++ src/execution/error_exec.c
@@ -30,7 +30,8 @@ int	perror_return(char *message, int return_value)
 
 int	end_single_cmd(t_data *data, int *io_fd, int *save, int return_val)
 {
-	close_free_token_env(data, io_fd[0], io_fd[1]);
+	free_env(data->ls_env);
+	close_all(io_fd[0], io_fd[1]);
 	close_all(save[0], save[1]);
 	if (return_val == ERROR_PROMPT)
 		return (ERR);
