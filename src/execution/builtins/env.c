e092b75621a98bb1732703a7c461387a46cf336d
Angele Barzilai <abarzila@z1r11p5.42lyon.fr>
2025-07-03 11:02:00 +0200

    export no arg OK export no '=' OK env OK

--- src/execution/builtins/env.c
+++ src/execution/builtins/env.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-void	exec_env(t_env *ls_env)
+void	exec_env_export(t_env *ls_env)
 {
 	t_env	*current;
 
@@ -16,3 +16,22 @@ void	exec_env(t_env *ls_env)
 		current = current->next;
 	}
 }
+
+void	exec_env(t_env *ls_env)
+{
+	t_env	*current;
+
+	if (!ls_env)
+	{
+		printf("Error: Environment list is NULL\n");
+		return ;
+	}
+	current = ls_env;
+	while (current)
+	{
+		if (ft_strchr(current->line, '='))
+			printf("%s\n", current->line);
+		current = current->next;
+	}
+}
+
