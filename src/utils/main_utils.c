9f3b8c97200184f77b758cb43e7d180e1af4948d
Angele Barzilai <abarzila@z1r6p4.42lyon.fr>
2025-07-17 10:07:12 +0200

    128/138

--- src/utils/main_utils.c
+++ src/utils/main_utils.c
@@ -28,6 +28,7 @@ void	parse_and_execute(t_parsing *parsing, t_data *data)
 {
 	add_history(parsing->prompt);
 	ft_parsing(data, parsing);
+	print_tokens(data);
 	if (parsing->errcode == OK)
 		execution(data);
 }
